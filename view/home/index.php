3f57cd62b2179b655d401619a3b621ed8b815c18
DESKTOP-DF69P9I\joeyd <99045878@mydavinci.nl>
2018-03-28 11:29:00 +0200

    Afgemaakt opnieuw

--- view/home/index.php
+++ view/home/index.php
@@ -1,28 +1,5 @@
 <div class="container">
-	<table border="1">
-		<tr>
-			<th>#</th>
-			<th>Naam</th>
-			<th>Dag</th>
-			<th>Maand</th>
-			<th>Jaar</th>
-			<th></th>
-		</tr>
-			<?php 
-				foreach ($birthdays as $birthday) {
-					echo "<tr>";
-					echo "<td>" . $birthday['id'] . "</td>";
-					echo "<td><a href='edit/". $birthday['id'] ."'>" . $birthday['person'] . "</a></td>";
-					echo "<td>" . $birthday['day'] . "</td>";
-					echo "<td>" . $birthday['month'] . "</td>";
-					echo "<td>" . $birthday['year'] . "</td>";
-					echo "<td><a class='remove' href='delete/". $birthday['id'] ."'>X</a></td>";
-					echo "</tr>";
-				}
-			?>
-			</tr>
-	</table>
-
+	<h2> Welkom op mijn kalender </h2>
 </div>
 
 <style>
